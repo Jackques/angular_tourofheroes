354e2930e413db92b67e1700da01d5c792a6b386
ANVA_DOMEIN\JackG <j.gommeren@anva.nl>
2019-10-25 18:01:24 +0200

    Updated app with routes and added routing module

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './../heroes/heroes.component';
+
+// Routes tell the Router which view to display when a user clicks a link or pastes a URL into the browser address bar.
+// Routes is a default type of variabele defined in the @angular router module
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  // the line below adds the RouterModule to the AppRoutingModule imports array and
+  // configures it with the routes in one step by calling RouterModule.forRoot():
+  imports: [RouterModule.forRoot(routes)],
+  // AppRoutingModule exports RouterModule so it will be available throughout the app.
+  exports: [RouterModule]
 })
+/*
+The method is called forRoot() because you configure the router at the application's root level.
+The forRoot() method supplies the service providers and directives needed for routing,
+and performs the initial navigation based on the current browser URL.
+ */
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,18 @@ import { HeroesComponent } from './heroes/heroes.component'; // this was already
 
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component'; // <-- NgModel lives here
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from "./app-routing/app-routing.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent // this was already set by the tutorial
+    MessagesComponent,
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule
   ],
