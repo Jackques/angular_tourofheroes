3c1176f717adfce4cfb20c5e548a9c871f192f12
ANVA_DOMEIN\JackG <j.gommeren@anva.nl>
2019-11-14 12:46:25 +0100

    Added Angular configuration and logic for in memory data service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,32 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component'; // this was already set by the tutorial
 import { MessagesComponent } from './messages/messages.component';
+import { HttpClientModule }    from '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppRoutingModule } from "./app-routing/app-routing.module";
+HttpClientInMemoryWebApiModule.forRoot(
+  InMemoryDataService, { dataEncapsulation: false }
+  // The forRoot() configuration method takes an InMemoryDataService class that primes the in-memory database
+)
 
 @NgModule({
   imports: [
     AppRoutingModule,
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+
+    // The forRoot() configuration method takes an InMemoryDataService class that primes the in-memory database.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   declarations: [
     AppComponent,
